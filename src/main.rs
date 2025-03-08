a3cbb6a4c5ec6c5ac1e1efda79651a0da45e2bcd
frederic.taillandier <frederic.taillandier@advertima.com>
2025-03-08 12:10:30 +0100

    sending a notification once a day

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use chrono::{Datelike, TimeZone, Timelike};
 use std::env;
 use std::error::Error;
-use std::time::Duration;
 use teloxide::prelude::*;
 use teloxide::types::ChatId;
 use tokio::time::interval;
@@ -25,15 +25,37 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     Ok(())
 }
 
+async fn wait_to_trigger_hour() {
+    let trigger_time = chrono::NaiveTime::from_hms_opt(12, 5, 0).unwrap();
+    let now = chrono::Local::now();
+    let next_notif_day = if now.time() >= trigger_time {
+        now + chrono::Duration::days(1)
+    } else {
+        now
+    };
+    let next_notif_time = chrono::Local
+        .with_ymd_and_hms(
+            next_notif_day.year(),
+            next_notif_day.month(),
+            next_notif_day.day(),
+            trigger_time.hour(),
+            trigger_time.minute(),
+            trigger_time.second(),
+        )
+        .unwrap();
+    let duration_to_trigger = next_notif_time.signed_duration_since(now);
+    let interval = duration_to_trigger.to_std().unwrap();
+    tokio::time::sleep(interval).await;
+}
+
 // Function to send messages on a schedule
 async fn send_scheduled_messages(
     bot: Bot,
     chat_id: ChatId,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let mut interval = interval(Duration::from_secs(10));
-
     loop {
-        interval.tick().await;
+        wait_to_trigger_hour().await;
+
         match bot.send_message(chat_id, "Hello World!").await {
             Ok(_) => println!("Scheduled message sent successfully"),
             Err(e) => eprintln!("Error sending scheduled message: {}", e),
