d9e72d67a9911aaffbea93b98a8f4ceb2ca630c5
frederic.taillandier <frederic.taillandier@advertima.com>
2025-05-01 00:34:52 +0200

    refactoring answer handlers in its own module

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use teloxide::prelude::*;
 mod data_grabber;
 mod email;
 mod telegram_writer;
+mod answer_handler;
 
 use teloxide::{
     payloads::SendMessageSetters,
@@ -88,7 +89,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let scheduled_task = tokio::spawn(send_scheduled_messages(app, task_state, bot.clone()));
 
     let message_handler = Update::filter_message().endpoint(handle_message);
-    let callback_handler = Update::filter_callback_query().endpoint(handle_callback_query);
+    let callback_handler = Update::filter_callback_query().endpoint(answer_handler::handle_callback_query);
     let handler = dptree::entry()
         .branch(message_handler)
         .branch(callback_handler);
@@ -221,124 +222,5 @@ async fn handle_message(bot: Bot, msg: Message) -> ResponseResult<()> {
                 .await?;
         }
     }
-    Ok(())
-}
-
-// Add this new handler function for callback queries
-async fn handle_callback_query(
-    bot: Bot,
-    query: CallbackQuery,
-    task_state: std::sync::Arc<std::sync::Mutex<SharedTaskState>>,
-) -> ResponseResult<()> {
-    // Extract the callback data from the query
-    if let Some(data) = &query.data {
-        // Get the chat ID from the message
-        if let Some(message) = query.message {
-            let chat_id = message.chat().id;
-
-            match data.as_str() {
-                "done" => {
-                    // Handle "Done" button
-                    let _ = bot
-                        .edit_message_text(
-                            chat_id,
-                            message.id(),
-                            "Thank you! Have a nice evening. <3",
-                        )
-                        .await?;
-                    let mut task_state = task_state.lock().unwrap();
-                    if task_state.state != TaskState::Pending {
-                        return Ok(());
-                    }
-                    task_state.state = TaskState::None;
-                    task_state.reminders_sent = 0;
-                    // set next trigger to tomorrow 18:00
-                    let tomorrow = chrono::Local::now() + chrono::Duration::days(1);
-                    let tomorrow_evening = chrono::Local
-                        .with_ymd_and_hms(
-                            tomorrow.year(),
-                            tomorrow.month(),
-                            tomorrow.day(),
-                            18,
-                            00,
-                            00,
-                        )
-                        .unwrap();
-                    task_state.next_trigger = tomorrow_evening;
-                }
-                "cant" => {
-                    // Handle "I can't" button
-                    bot.edit_message_text(
-                        chat_id,
-                        message.id(),
-                        "No problem. I will ask the others to help.",
-                    )
-                    .await?;
-                    let mut task_state = task_state.lock().unwrap();
-                    if task_state.state != TaskState::Pending {
-                        return Ok(());
-                    }
-                    task_state.state = TaskState::Failed;
-                    task_state.reminders_sent = 0;
-                    // set next trigger to now
-                    task_state.next_trigger = chrono::Local::now();
-                }
-                "new_bags" => {
-                    // Handle "No bags" button
-                    let keyboard = InlineKeyboardMarkup::new(vec![
-                        // First row with two buttons
-                        vec![InlineKeyboardButton::callback("NEW BAGS !!!", "sure_bags")],
-                        vec![InlineKeyboardButton::callback(
-                            "Nah, no need",
-                            "enough_bags",
-                        )],
-                    ]);
-
-                    bot.edit_message_text(
-                        chat_id,
-                        message.id(),
-                        "Are you sure ? A request will be sent to We-Recycle.",
-                    )
-                    .reply_markup(keyboard)
-                    .await?;
-                }
-                "sure_bags" => {
-                    // Handle "Sure bags" button
-                    bot.edit_message_text(
-                        chat_id,
-                        message.id(),
-                        "Thank you! I sent a request to We-Recycle.",
-                    )
-                    .await?;
-                    email::request_new_bags();
-                }
-                "enough_bags" => {
-                    // Handle "Enough bags" button
-                    bot.edit_message_text(chat_id, message.id(), "Great! Have a nice evening.")
-                        .await?;
-                }
-                "aaa" => {
-                    // Handle "AAA" button
-                    bot.edit_message_text(chat_id, message.id(), "You clicked AAA!")
-                        .await?;
-                }
-                "bbb" => {
-                    // Handle "BBB" button
-                    bot.edit_message_text(chat_id, message.id(), "You clicked BBB!")
-                        .await?;
-                    email::request_new_bags();
-                }
-
-                _ => {
-                    // Handle unknown callback data
-                    bot.send_message(chat_id, "Unrecognized option.").await?;
-                }
-            }
-
-            // Answer the callback query to remove the "loading" state
-            bot.answer_callback_query(query.id).await?;
-        }
-    }
-
     Ok(())
 }
