57eef57e09b9baf8a18253b2b51bf7bd78ad491d
Review Agent <frederic.taillandier@advertima.com>
2026-06-02 21:41:21 +0200

    Fix missing verb in daily trash reminder message

--- src/telegram_writer.rs
+++ src/telegram_writer.rs
@@ -80,7 +80,7 @@ async fn daily_update(
                 .iter()
                 .fold(String::new(), |acc, trash| format!("{} {}", acc, trash));
             let daily_update_txt = format!(
-                "Hello {} !\nDon't forget the{} trashes out before tomorrow morning! \n\
+                "Hello {} !\nDon't forget to put the{} trashes out before tomorrow morning! \n\
                 If you don't answer this message before 9pm,\n\
                 a reminder will be sent to all the flatmates.\n\
                 ",
